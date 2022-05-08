c537d7a42d32d620be262938506c6c84706b8fba
Juan-Baldelomar <jlbc95@gmail.com>
2022-05-07 21:38:43 -0500

    load weights fixed. Model and Optimizer need a train_step to init weights and then load the weights

--- Code/training/training.py
+++ Code/training/training.py
@@ -49,13 +49,14 @@ def save_optimizer(optimizer, weights_path, config_path):
     save_pkl_data(optimizer.get_config(), config_path, 4)
 
 
-def load_optimizer(weights_path, config_path):
+def load_optimizer(weights_path, config_path, model, inputs, stds):
     conf = load_pkl_data(config_path)
     weights = load_pkl_data(weights_path)
     # load custom scheduler
     conf['learning_rate'] = CustomSchedule.from_config(conf['learning_rate']['config'])
     # load optimizer conf and weights
     optimizer = tf.keras.optimizers.Adam.from_config(conf)
+    model.train_step(*inputs, stds, [], optimizer)
     optimizer.set_weights(weights)
     return optimizer
 
@@ -93,7 +94,7 @@ def split_params(params):
         'preload': params.get('preload', False),
         'model_path': params.get('model_path') ,
         'opt_weights_path': params.get('opt_weights_path'),
-        'opt_conf_path': params.get('opt_config_path')
+        'opt_conf_path': params.get('opt_conf_path')
     }
 
     data_params = {
@@ -104,17 +105,19 @@ def split_params(params):
     return model_params, batch, epochs, preload_params, data_params
 
 
-def load_model_and_opt(model_params, preload=False, model_path=None, opt_weights_path=None, opt_config_path=None):
+def load_model_and_opt(model_params, dataset, stds, preload=False, model_path=None, opt_weights_path=None, opt_config_path=None):
     # build model
     model = STTransformer(**model_params)
     # load model if possible
     if preload:
         if model_path is not None and valid_paths(model_path):
+            past, future, maps, _ = next(iter(dataset)) 
+            model((past, future, maps), False, stds)
             model.set_weights(load_pkl_data(model_path))
         # load optimizer if possible
         if opt_config_path is not None and opt_weights_path is not None \
                 and valid_paths(opt_config_path, opt_weights_path):
-            optimizer = load_optimizer(opt_weights_path, opt_config_path)
+            optimizer = load_optimizer(opt_weights_path, opt_config_path, model, (past, future, maps), stds)
     else:
         learning_rate = CustomSchedule(256)
         optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.99, beta_2=0.9, epsilon=1e-9)
@@ -139,7 +142,7 @@ def train(model_params, batch, epochs, data_path, maps_dir, preload=False, model
     data = load_pkl_data(data_path)
     dataset, std_x, std_y = buildDataset(data, batch, pre_path=maps_dir)
     stds = tf.constant([[[[std_x, std_y]]]], dtype=tf.float32)
-    model, optimizer = load_model_and_opt(model_params, preload, model_path, opt_weights_path, opt_conf_path)
+    model, optimizer = load_model_and_opt(model_params, dataset, stds, preload, model_path, opt_weights_path, opt_conf_path)
 
     worst_loss = np.inf
     for epoch in range(epochs):
