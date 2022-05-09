443482cdbcbcd7c2fbef7c56a335fe7ad7e5dffc
Juan Luis <jlbc95@gmail.com>
2022-05-09 01:01:13 -0500

    stamp positions by batch now implemented

--- Code/dataset/dataset.py
+++ Code/dataset/dataset.py
@@ -51,6 +51,52 @@ def stamp_positions_in_bitmap(inputs: np.ndarray, masks: np.ndarray, bitmaps: np
     return neigh_bitmaps
 
 
+def stamp_positions_by_batch(inputs: np.ndarray, masks: np.ndarray, bitmaps: np.ndarray,
+                             pixbymeter: float, yaw, step_start=-1, step_end=1, debug=False):
+    """
+    :param: inputs    : np array of the form (S, N, F). S=sequence, N=Batch * Neighbors, F=Features (x=0, y=1).
+    :param: masks     : np array of the form (S, N). S=sequence, N=Batch * Neighbors. Indicates thoses entries that are padded.
+    :param: bitmaps   : np array of the form (L, H, W). L=Layers, H=Height, W=Width.
+    :param: pixbymeter: relation of number of pixels by each meter
+    :param: yaw       : (B * N, S) rotation angle of the points (if map was rotated, trajectories should be rotated by the same angle)
+    :param: step start: when stamping the step of and agent, a pixel width might be to small, so you can make it bigger by setting the step_start
+                        and step_end, so you stamp all the pixels in the range (pos + step_start, pos + step_end)
+    :param: debug     : if True, print values of pixels
+    """
+    assert(step_start <= 0 and step_end >= 0)
+    # needed shapes
+    S, N, _ = inputs.shape
+    nxb, n_layers, H, W = bitmaps.shape
+    neigh_bitmaps = bitmaps
+    # center pixels
+    x_p, y_p = H / 2., W / 2.
+    # get x, y for each observation, select the points that are not padded (masks==0) and get array flattened
+    x = inputs[:, :, 0][masks == 0]
+    y = inputs[:, :, 1][masks == 0]
+    # perform rotation (clockwise)
+    pix_x = x * np.cos(yaw[masks == 0]) + y * np.sin(yaw[masks == 0])
+    pix_y = -x * np.sin(yaw[masks == 0]) + y * np.cos(yaw[masks == 0])
+    # transform to pixel position
+    pix_x = (pix_x * pixbymeter + x_p).astype(np.int32)
+    pix_y = (pix_y * pixbymeter + y_p).astype(np.int32)
+    pix_x = np.maximum(0 - step_start, np.minimum(255 - step_end, pix_x))
+    pix_y = np.maximum(0 - step_start, np.minimum(255 - step_end, pix_y))
+    # get neighbor dimension indexes, select the ones that are not padded and get array flattened
+    N_pos = np.arange(N)[np.newaxis, :] * np.ones([S, N]).astype(np.int32)
+    N_pos = N_pos[masks == 0]
+    # build positions layers
+    # stamp values
+    for i in range(step_start, step_end + 1):
+        for j in range(step_start, step_end + 1):
+            neigh_bitmaps[:, 2, :, :][(N_pos, pix_y + i, pix_x + j)] = 255.0
+    # append new layer
+    if debug:
+        print('x: ', np.reshape(pix_x, (S, N)))
+        print('y: ', np.reshape(pix_y, (S, N)))
+
+    return neigh_bitmaps
+
+
 def adapt_spa_mask(mask):
     return mask[np.newaxis, :, np.newaxis, :].astype(np.float32)   # (<new axis head>, seq, <new axis neighbor>, neighbors)
                                                                    # to broadcast when doing addition in the attention layer
--- Code/dataset/test.py
+++ Code/dataset/test.py
@@ -5,7 +5,7 @@ from shifts_dataloader import ShiftsLoader
 from InputQuery import *
 from InputQuery import verifyNan
 from InputQuery import process_nans
-from Code.dataset.dataset import stamp_positions_in_bitmap
+from Code.dataset.dataset import stamp_positions_in_bitmap, buildDataset
 from Code.utils import save_utils as dl
 
 # PATH
@@ -67,17 +67,17 @@ data_name = 'train'
 # #             break
 
 # -------------------------------------------------------------------- SHIFTS --------------------------------------------------------------------
-dataroot = '/data/shifts/data/development'
-chunk_number = 10
-pickle_filename = '/data/shifts/data_chunk' + str(chunk_number) + '.pkl'
-maps_path = '../data/maps/shifts/chunk' + str(chunk_number)
-final_path = '../data/shifts_data_chunk' + str(chunk_number) + '.pkl'
-chunk_start = (chunk_number-1) * 2000
-chunk_end = chunk_number * 2000
-shifts_loader = ShiftsLoader(DATAROOT=dataroot, pickle=True, pickle_filename=pickle_filename, chunk=(chunk_start, chunk_end))
-inputQuery = InputQuery(shifts_loader)
-shifts_bitmap = ShiftsBitmap()
-inputs = inputQuery.get_TransformerCube_Input(25, 25, 5, 24, bitmap_extractor=None, path=maps_path)
+# dataroot = '/data/shifts/data/development'
+# chunk_number = 10
+# pickle_filename = '/data/shifts/data_chunk' + str(chunk_number) + '.pkl'
+# maps_path = '../data/maps/shifts/chunk' + str(chunk_number)
+# final_path = '../data/shifts_data_chunk' + str(chunk_number) + '.pkl'
+# chunk_start = (chunk_number-1) * 2000
+# chunk_end = chunk_number * 2000
+# shifts_loader = ShiftsLoader(DATAROOT=dataroot, pickle=True, pickle_filename=pickle_filename, chunk=(chunk_start, chunk_end))
+# inputQuery = InputQuery(shifts_loader)
+# shifts_bitmap = ShiftsBitmap()
+# inputs = inputQuery.get_TransformerCube_Input(25, 25, 5, 24, bitmap_extractor=None, path=maps_path)
 #dl.save_pkl_data(inputs, final_path)
 
 
@@ -97,9 +97,9 @@ inputs = inputQuery.get_TransformerCube_Input(25, 25, 5, 24, bitmap_extractor=No
 
 
 # --------------------------------------- ******* TEST visualize maps and stamped positions ******* ---------------------------------------
-file_id = inputs[1225]['ego_id'] + '.npz'
-ego_id, _ = file_id.split('_')
-ego_vehicle = shifts_loader.dataset.ego_vehicles[ego_id]
+#file_id = inputs[1225]['ego_id'] + '.npz'
+#ego_id, _ = file_id.split('_')
+#ego_vehicle = shifts_loader.dataset.ego_vehicles[ego_id]
 
 # img_comp = np.load('../data/maps/shifts/chunk10/' + file_id)
 # img = img_comp['bitmaps']
@@ -108,16 +108,16 @@ ego_vehicle = shifts_loader.dataset.ego_vehicles[ego_id]
 # plt.imshow(img[:, :, 1])
 # plt.show()
 #
-inps = inputQuery.get_egocentered_input(ego_vehicle, shifts_loader.dataset.agents, 50, 5, 0,
-                                        24, bitmap_extractor=shifts_bitmap)
+#inps = inputQuery.get_egocentered_input(ego_vehicle, shifts_loader.dataset.agents, 50, 5, 0,
+#                                        24, bitmap_extractor=shifts_bitmap)
 
 #
-yaw = list(ego_vehicle.timesteps.values())[24].rot
-bitmaps = stamp_positions_in_bitmap(inps[0][0:25], inps[1][0:25], inps[2], 256 / 200.0, yaw)
-bitmaps = np.transpose(bitmaps, [0, 2, 3, 1])
-plt.figure(figsize=(10, 10))
-plt.imshow(bitmaps[0])
-plt.show()
+# yaw = list(ego_vehicle.timesteps.values())[24].rot
+# bitmaps = stamp_positions_in_bitmap(inps[0][0:25], inps[1][0:25], inps[2], 256 / 200.0, yaw)
+# bitmaps = np.transpose(bitmaps, [0, 2, 3, 1])
+# plt.figure(figsize=(10, 10))
+# plt.imshow(bitmaps[0])
+# plt.show()
 
 # ------------------------------------------------------------------------- TEST -------------------------------------------------------------------------
 
@@ -144,3 +144,48 @@ plt.show()
 # matrixes, ids = inputQuery.get_single_Input(7, 8, 7)
 #
 # dl.save_pkl_data(matrixes, 'val_nusc_inps.pkl')
+
+
+import tensorflow as tf
+data = dl.load_pkl_data('../data/shifts_data_chunk10.pkl')
+dataset = buildDataset(data, 8, pre_path='../data/maps/shifts/chunk10/')
+
+data_iter = iter(dataset[0])
+past, future, maps, target = next(data_iter)
+
+past_inp = target[0][:, 1:, :, :]
+past_mask = tf.squeeze(future[2])[:, 1:, :]
+yaws = target[2]
+yaws = yaws[:, tf.newaxis, tf.newaxis] * tf.ones([8, 25, 5])
+yaws = tf.transpose(yaws, [0, 2, 1])
+yaws = tf.reshape(yaws, [-1, 25])
+yaws = tf.transpose(yaws, [1, 0])
+
+past_inp = tf.transpose(past_inp, [0, 2, 1, 3])
+past_mask = tf.transpose(past_mask, [0, 2, 1])
+
+past_inp = tf.reshape(past_inp, [-1, 25, 3])
+past_mask = tf.reshape(past_mask, [-1, 25])
+
+past_inp = tf.transpose(past_inp, [1, 0, 2])
+past_mask = tf.transpose(past_mask, [1, 0])
+
+#neigh_bitmaps = np.ones([8, 5, 2, 256, 256]) * maps[:, np.newaxis, :, :, :]
+neigh_bitmaps = tf.transpose(maps, [0, 1, 4, 2, 3])
+neigh_bitmaps = tf.reshape(neigh_bitmaps, [-1, 3, 256, 256])
+
+bitmaps = stamp_positions_in_bitmap(past_inp.numpy(), past_mask.numpy(), neigh_bitmaps.numpy(), 256.0/200, yaws.numpy(), mode=2)
+#bitmaps = bitmaps.numpy()
+bitmaps = np.reshape(bitmaps, [-1, 5, 3, 256, 256])
+#bitmaps_final_layer = bitmaps[:, :, 2, :, :] + bitmaps[:, :, 3, :, :]
+
+#bitmaps[:, :, 2, :, :] = bitmaps_final_layer
+bitmaps = np.transpose(bitmaps, [0, 1, 3, 4, 2])
+
+plt.figure(figsize=(10, 10))
+plt.imshow(bitmaps[1, 0, :, :, :3])
+plt.show()
+
+plt.imshow(maps[1, 0, :, :, :3])
+plt.show()
+
