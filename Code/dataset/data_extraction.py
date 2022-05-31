94d70ae5e955000cb15be46e9a85f41f8b5cf5f4
Juan Luis <jlbc95@gmail.com>
2022-05-31 13:35:29 -0500

    data extraction refactor to support more neighbors and create needed directories

--- Code/dataset/data_extraction.py
+++ Code/dataset/data_extraction.py
@@ -6,20 +6,33 @@ from InputQuery import *
 from Code.utils import save_utils as dl
 
 
+# def data_statistics(data_start, data_end):
+#     assert (data_start >= 1 and data_end <= 18)
+#     dataroot = '/data/shifts/data/development'
+#
+#     for chunk_number in range(data_start, data_end + 1):
+#         # path of preloaded data
+#         pickle_filename = '/data/shifts/data_chunk' + str(chunk_number) + '.pkl'
+#         chunk_start = (chunk_number - 1) * 2000
+#         chunk_end = chunk_number * 2000
+#         shifts_loader = ShiftsLoader(DATAROOT=dataroot, pickle=pickle, pickle_filename=pickle_filename,
+#                                      chunk=(chunk_start, chunk_end))
+
+
 def shifts_extraction(past_length, future_length, neighbors, get_bitmaps: bool, origin_offset=None,
                       pickle=True, data_start=1, data_end=18, force_overwrite=False):
-    assert(data_start >= 1 and data_end <= 18)
-    dataroot = '/data/shifts/data/development'
+    assert(data_start >= 1 and data_end <= 188)
+    dataroot = '/data/shifts/data/train'
 
     if origin_offset is None:
         origin_offset = past_length - 1
 
     for chunk_number in range(data_start, data_end + 1):
         # path of preloaded data
-        pickle_filename = '/data/shifts/data_chunk' + str(chunk_number) + '.pkl'
+        pickle_filename = '/data/shifts/train/data_chunk' + str(chunk_number) + '.pkl'
         # path to store inputs
-        maps_path  = '../data/shifts/neigh_' + str(neighbors) + '/maps/chunk' + str(chunk_number)
-        final_path = '../data/shifts/neigh_' + str(neighbors) + '/shifts_data_chunk' + str(chunk_number) + '.pkl'
+        maps_path  = '../data/shifts/train/neigh_' + str(neighbors) + '/maps/chunk' + str(chunk_number)
+        final_path = '../data/shifts/train/neigh_' + str(neighbors) + '/shifts_data_chunk' + str(chunk_number) + '.pkl'
         # create needed directories
         dl.valid_path(maps_path, os.path.dirname(final_path))
         # START EXTRACTION
@@ -43,17 +56,24 @@ def shifts_extraction(past_length, future_length, neighbors, get_bitmaps: bool, 
             dl.save_pkl_data(inputs, final_path)
 
 
-def join_data(prepath, start, end, neigh):
-    assert(start >= 1 and end <= 18)
+def join_data(prepath, start, end):
+    assert(start >= 1 and end <= 188)
     inputs = []
     for i in range(start, end+1):
+        print('joining file:', i)
         path = prepath + str(i) + '.pkl'
         input_ = dl.load_pkl_data(path)
         inputs += input_
 
-    dl.save_pkl_data(inputs, '../data/shifts/neigh_' + str(neigh) + '/shifts_data_all_p4.pkl', protocol=4)
+    dl.save_pkl_data(inputs, '../data/shifts/train/neigh_5/shifts_data_all_p2_v4.pkl', protocol=4)
 
 
-shifts_extraction(past_length=25, future_length=25, neighbors=5, get_bitmaps=True, pickle=False, data_start=1, data_end=2)
+#shifts_extraction(past_length=25, future_length=25, neighbors=5, get_bitmaps=True, pickle=False, data_start=83, data_end=83)
 
-join_data('../data/shifts_data_chunk', start=1, end=18, neigh=5)
+#join_data('../data/shifts/train/neigh_5/shifts_data_chunk', start=95, end=188)
+
+data1 = dl.load_pkl_data('../data/shifts/train/neigh_5/shifts_data_all_p1_v4.pkl')
+data2 = dl.load_pkl_data('../data/shifts/train/neigh_5/shifts_data_all_p2_v4.pkl')
+final_data = data1 + data2
+
+dl.save_pkl_data(final_data, '../data/shifts/train/neigh_5/shifts_data_all_p4.pkl')
